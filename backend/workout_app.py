3da4c01a85cd09cc9edf240b09eef71495122497
Brian Zhang <bz88keys@gmail.com>
2021-09-18 11:43:10 -0400

    add exercises file

--- backend/workout_app.py
+++ backend/workout_app.py
@@ -1,19 +1,21 @@
 import cv2
 import mediapipe as mp
+import math
 from pose_detection import *
-
+from exercises import *
 def main():
     cap = cv2.VideoCapture(0)
     detector = pose_detector()
+    cur_session = {'reps': 0, 'up': True}
     while True:
         ret, img = cap.read()
         lmList = detector.find_position(img)
         
         if len(lmList) != 0:
             #print(lmList['right_pinky'], lmList['right_wrist'], lmList['right_thumb'])
-            print(detector.calc_angle(lmList['right_shoulder'], lmList['right_elbow'], lmList['right_wrist']))
-            #print(detector.calc_cos_angle([1,0,0],[0,0,0],[0,1,0]))
-        
+            count_bicep_curl(cur_session, detector,lmList)
+            
+            print(math.floor(cur_session['reps']))
         cv2.imshow("hello",detector.find_pose(img))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break;
